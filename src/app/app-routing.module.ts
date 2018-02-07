225c5fef180bbc4624e2c3811212dc347c13f34e
Chris Healy <chrishealy871@gmail.com>
2018-02-07 17:53:35 +0000

    Fixed redirection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
 import {ModuleWithProviders} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {PageNotFoundComponent} from './component/page-not-found/page-not-found.component';
 import {HomeComponent} from './component/home/home.component';
 import {AboutComponent} from './component/about/about.component';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'about', component: AboutComponent},
-  {path: '**', component: PageNotFoundComponent}
+  {path: '**', component: HomeComponent}
 ];
 
 export const Routing: ModuleWithProviders = RouterModule.forRoot(appRoutes, {useHash: true});
